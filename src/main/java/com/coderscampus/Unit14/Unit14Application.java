840c96eebd13fe5efad475b99e3b83e9dc9f68b4
Dave Naugler <davenaugler@gmail.com>
2024-01-02 15:53:57 -0700

    🚀 Initial commit

---
+++ src/main/java/com/coderscampus/Unit14/Unit14Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Unit14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit14Application.class, args);
+	}
+
+}
