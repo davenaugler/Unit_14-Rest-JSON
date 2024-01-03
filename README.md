c1c38cbf8ce5d84af58cab46c7d2b4569cd5792f
Dave Naugler <davenaugler@gmail.com>
2024-01-02 17:49:55 -0700

    🚀 Commit after Lesson 1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Unit_14-Rest-JSON
+
+## 01 - JSON Lesson
+- Created `JsonExample.java` and broke down how JSON looks and how it is structured.
+- JSON is based on Key Value pairs
+- Objects are represented by curly brackets `{}`
+- Arrays are represented by square brackets `[]`
+
+## 02 - Converting JSON Strings to Java and Back
+- Downloading someone else's code, 
+  - Taking a Java Object and converting it into a JSON String and visa versa
---
+++ src/main/java/com/coderscampus/Unit14/JsonExample.java
@@ -0,0 +1,50 @@
+package com.coderscampus.Unit14;
+
+public class JsonExample {
+
+    /**
+     *? JSON
+     * ? JavaScript Object Notation
+     * ? A simple String based way to notate data
+     ** - Key : Value pairs
+     *!   - Key : Value pairs are seperated by commas
+     *!   - Objects are represented by curly brackets {}
+     *!   - Arrays are represented by square brackets []
+     *
+     *? Important note: JSON is not tied to JavaScript... it is
+     *?   language agnostic (meaning it'll work in any language)
+     *
+     ** JSON is just data passed around on the internet.
+     ** From client to server then from server to client.
+     ** It is the accepted way of passing data back-and-fourth from client to server.
+     ** Or from one language to another via an API Endpoint
+     *
+     *? Person: name, age, gender (Java object)
+     *! JSON example of Person
+     *? {
+     *?     "name"    : "Trevor Page",
+     *?      "age"    : 37,
+     *?      "gender" : "Male"
+     *?}
+     *
+     *! Remember List is just an Array
+     *
+     *? List<Person> people
+     *! JSON example of List<Person> people
+     *? [
+     *?  {
+     *?      "name"   : "Trevor Page",
+     *?      "age"    : 37,
+     *?      "gender" : "Male"
+     *?  },
+     *?  {
+     *?      "name"   : "Jane Doe",
+     *?      "age"    : 40,
+     *?      "gender" : "Female"
+     *?  }
+     *? ]
+     */
+
+
+
+}
